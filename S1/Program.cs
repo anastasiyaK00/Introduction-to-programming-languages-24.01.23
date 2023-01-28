f2f370913c2807ba1fb4f6ef1bfd64d95b8b139d
Anastasiya <anastasiyak96.96@mail.ru>
2023-01-28 11:02:22 +0300

    S1

---
+++ S1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти максимальное и минимальное - вывести на экран
+
+Console.WriteLine("Enter 1st number: ");
+int First = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter 2nd number: ");
+int Second = int.Parse(Console.ReadLine());
+
+if(First > Second)
+{
+    Console.WriteLine("Bigger:" + First);
+    Console.WriteLine("Smaller: " + Second);
+}
+else 
+{
+    Console.WriteLine("Bigger:" + Second);
+    Console.WriteLine("Smaller:" + First);
+}
